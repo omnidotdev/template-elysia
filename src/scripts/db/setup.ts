a16f0e6190bf51411f5738b16f9aca12428037a0
Brian Cooper <brian@brian-cooper.com>
2025-05-21 16:19:42 -0500

    fix(scripts,db,setup): set dynamic database name

--- src/scripts/db/setup.ts
+++ src/scripts/db/setup.ts
@@ -8,6 +8,6 @@ import { DATABASE_NAME } from "lib/config/env.config";
 
 // biome-ignore lint/suspicious/noConsoleLog: script logging
 console.log(`Creating ${DATABASE_NAME} database...`);
-await $`createdb -U postgres template-elysia`;
+await $`createdb -U postgres ${DATABASE_NAME}`;
 // biome-ignore lint/suspicious/noConsoleLog: script logging
 console.log("Database created");
