0cab4b0b81b616da3d55b81cfa92fd42504dc4e6
Brian Cooper <brian@brian-cooper.com>
2025-05-20 21:14:29 -0500

    feature(graphile): add connection filter plugin

--- src/lib/config/graphile.config.ts
+++ src/lib/config/graphile.config.ts
@@ -1,4 +1,5 @@
 import { PgSimplifyInflectionPreset } from "@graphile/simplify-inflection";
+import PostGraphileConnectionFilterPreset from "postgraphile-plugin-connection-filter";
 import { makePgService } from "postgraphile/adaptors/pg";
 import { PostGraphileAmberPreset } from "postgraphile/presets/amber";
 
@@ -8,7 +9,11 @@ import { DATABASE_URL, isDevEnv } from "./env.config";
  * Graphile preset.
  */
 const graphilePreset: GraphileConfig.Preset = {
-  extends: [PostGraphileAmberPreset, PgSimplifyInflectionPreset],
+  extends: [
+    PostGraphileAmberPreset,
+    PgSimplifyInflectionPreset,
+    PostGraphileConnectionFilterPreset,
+  ],
   pgServices: [makePgService({ connectionString: DATABASE_URL })],
   grafast: { explain: isDevEnv },
 };
