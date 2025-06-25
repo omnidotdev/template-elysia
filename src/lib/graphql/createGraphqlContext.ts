d201caa409eefd6a996e98d0216392c3147b989c
Brian Cooper <brian@brian-cooper.com>
2025-06-25 14:14:44 -0500

    chore: format

--- src/lib/graphql/createGraphqlContext.ts
+++ src/lib/graphql/createGraphqlContext.ts
@@ -5,7 +5,10 @@ import { dbPool, pgPool } from "lib/db/db";
 import type { YogaInitialContext } from "graphql-yoga";
 import type { SelectUser } from "lib/db/schema";
 import type { WithPgClient } from "postgraphile/@dataplan/pg";
-import type { NodePostgresPgClient, PgSubscriber } from "postgraphile/adaptors/pg";
+import type {
+  NodePostgresPgClient,
+  PgSubscriber,
+} from "postgraphile/adaptors/pg";
 
 const withPgClient = createWithPgClient({ pool: pgPool });
 
